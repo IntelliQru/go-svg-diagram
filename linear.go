d9ac2617d5d2626f24d9e7c6dc559794158ffafd
Juggle <ara@intelliq.ru>
2016-01-29 20:12:03 +0300

    Update Linear diagram

---
+++ linear.go
@@ -0,0 +1,177 @@
+package svgd
+import (
+	"io"
+	"errors"
+	"fmt"
+	"github.com/ajstarks/svgo"
+)
+
+
+type LinearCategory struct {
+	Color  string
+	values []float64
+}
+
+func (lc *LinearCategory) SetValues(vals []float64)  {
+
+	lc.values = make([]float64, len(vals))
+	copy(lc.values, vals)
+}
+
+type LinearDiagram struct {
+	Title  string
+	Width  int
+	Height int
+	Grid bool
+	VStep  int
+
+	categories map[string]*LinearCategory
+	labels     []string
+}
+
+func (d *LinearDiagram) NewCategory(name string) (cat *LinearCategory) {
+	n := new(LinearCategory)
+
+	n.values = make([]float64, 0)
+	d.categories[name] = n
+
+	return n
+}
+
+
+
+func (d *LinearDiagram) SetLabels(labels []string) {
+
+	d.labels = make([]string, len(labels))
+	copy(d.labels, labels)
+}
+
+
+func (d *LinearDiagram) build(w io.Writer) (err error) {
+
+	var minValue, maxValue float64
+	isFirst := true
+
+	for name, cat := range d.categories {
+		if len(cat.values) != len(d.labels) {
+			err = errors.New(fmt.Sprintf("Error: Count of values for category '%s' does not match labels count",
+				name))
+			return
+		}
+
+		for iVal := 0; iVal < len(cat.values); iVal++ {
+			if maxValue < cat.values[iVal] {
+				maxValue = cat.values[iVal]
+			}
+			if isFirst {
+				minValue = cat.values[iVal]
+				isFirst = false
+			} else if minValue > cat.values[iVal] {
+				minValue = cat.values[iVal]
+			}
+		}
+	}
+
+	s := svg.New(w)
+	s.Start(d.Width, d.Height)
+
+	// Title
+	s.Text(d.Width/2, dsMarginTop/2, d.Title,
+		fmt.Sprintf("text-anchor:middle;font-size:%d;fill:%s", dsTitleFontSize, dsTitleFontColor))
+
+	// Y axis
+	s.Line(dsMarginLeft, d.Height-dsMarginBottom, dsMarginLeft, dsMarginTop,
+		fmt.Sprintf("stroke-width:%d;stroke:%s;", dsAxisLineWidth, dsAxisLineColor))
+	// X axis
+	s.Line(dsMarginLeft, d.Height-dsMarginBottom, d.Width-dsMarginRight, d.Height-dsMarginBottom,
+		fmt.Sprintf("stroke-width:%d;stroke:%s;", dsAxisLineWidth, dsAxisLineColor))
+
+	// Write labels
+	length := len(d.labels)
+	xStep := (d.Width - dsMarginLeft - dsMarginRight) / (length - 1)
+	left := dsMarginLeft
+
+	s.Group(fmt.Sprintf("text-anchor:middle;font-size:%d;fill:%s", dsLabelsFontSize, dsLabelsFontColor))
+	for i := 0; i < length; i++ {
+		s.Text(left, d.Height-dsMarginBottom+dsLabelsMargin, d.labels[i])
+		left += xStep
+	}
+	s.Gend()
+
+	// Write Y values
+	// TODO round Y start for step nearest value
+	if d.VStep <= 0 {
+		err = errors.New(fmt.Sprintf("Error: Invalid VStep value '%d'", d.VStep))
+		return
+	}
+
+	yCount := float64(maxValue-minValue) * 1.1 / float64(d.VStep)
+	vCount := int(yCount + 0.5)
+	yStep := int(float64(d.Height-dsMarginTop-dsMarginBottom) / yCount)
+	val := int(minValue)
+	top := d.Height - dsMarginBottom
+
+	s.Group(fmt.Sprintf("text-anchor:end;font-size:%d;fill:%s", dsLabelsFontSize, dsLabelsFontColor))
+	for i := 0; i < vCount; i++ {
+		s.Text(dsMarginLeft-dsValuesMargin, top, fmt.Sprintf("%d", val))
+		val += d.VStep
+		top -= yStep
+	}
+	s.Gend()
+
+	// Drawing grid
+	if d.Grid {
+
+		s.Group("stroke-width:1;stroke:lightgray")
+
+		// Vertical grid
+		left = dsMarginLeft + xStep
+		for i := 1; i < length; i++ {
+
+			s.Line(left, dsMarginTop, left, d.Height-dsMarginBottom)
+			left += xStep
+		}
+
+		// Horizontal grid
+		top = d.Height - dsMarginBottom - yStep
+		for i := 1; i < vCount; i++ {
+			s.Line(dsMarginLeft, top, d.Width-dsMarginRight, top)
+			top -= yStep
+		}
+
+		s.Gend()
+	}
+
+	// Draw linear graphs
+
+	fmt.Println(minValue, maxValue)
+	fmt.Println(d.categories)
+
+	for _, cat := range d.categories {
+
+		s.Group(fmt.Sprintf("stroke-width:%d;stroke:%s", dsLinearWidth, cat.Color))
+
+		valLength := maxValue - minValue
+		yLength := float64(yStep*(vCount-1))
+		x1 := dsMarginLeft
+		y1 := d.Height - dsMarginBottom - int((cat.values[0]-minValue)/valLength*yLength)
+
+		length = len(cat.values)-1
+		for iVal := 0; iVal < length; iVal++ {
+
+			x2 := dsMarginLeft+(iVal+1)*xStep
+			y2 := d.Height - dsMarginBottom - int((cat.values[iVal+1]-minValue)/valLength*yLength)
+
+			s.Line(x1, y1, x2, y2)
+			fmt.Println(valLength, yLength, x1, y1, x2, y2)
+
+			y1 = y2
+			x1 = x2
+		}
+		s.Gend()
+	}
+
+	s.End()
+
+	return
+}
--- svgd.go
+++ svgd.go
@@ -1,19 +1,10 @@
 package svgd
 
 import (
-	"errors"
-	"fmt"
-	"github.com/ajstarks/svgo"
 	"io"
+	"errors"
 )
 
-type DiagramType int
-
-const (
-	DT_LINE DiagramType = iota
-	DT_PIE
-	DT_BAR
-)
 
 const (
 	dsMarginLeft   = 50
@@ -35,192 +26,40 @@ const (
 	dsLinearWidth = 3
 )
 
-type DiagramSettings struct {
-	Type   DiagramType
-	Width  int
-	Height int
-	Title  string
-	Grid   bool
-	VStep  int
-}
 
-type DiagramCategory struct {
-	Name   string
-	Color  string
-	Values []float64
+type diagramInterface interface {
+	build(w io.Writer) error
 }
 
 type Diagram struct {
-	dType  DiagramType
-	title  string
-	width  int
-	height int
 
-	isGrid bool
-	vStep  int
-
-	categories []DiagramCategory
-	labels     []string
+	diagram diagramInterface
 }
 
-func NewDiagram(settings DiagramSettings) *Diagram {
+func NewDiagram() *Diagram {
 
-	dg := Diagram{
-		title:      settings.Title,
-		dType:      settings.Type,
-		width:      settings.Width,
-		height:     settings.Height,
-		categories: make([]DiagramCategory, 0),
-		isGrid:     settings.Grid,
-		vStep:      settings.VStep,
-	}
+	dg := Diagram{}
 
 	return &dg
 }
 
-func (d *Diagram) AddCategory(cat DiagramCategory) {
+func (d *Diagram) CreateLinear() (dg *LinearDiagram) {
+	newLD := new(LinearDiagram)
+	newLD.categories = make(map[string]*LinearCategory, 0)
 
-	vals := make([]float64, len(cat.Values))
-	copy(vals, cat.Values)
+	d.diagram = newLD
 
-	newCat := DiagramCategory{
-		Name:   cat.Name,
-		Color:  cat.Color,
-		Values: vals,
-	}
-
-	d.categories = append(d.categories, newCat)
-
+	return newLD
 }
 
-func (d *Diagram) SetLabels(labels []string) {
-
-	d.labels = make([]string, len(labels))
-	copy(d.labels, labels)
-}
 
 func (d *Diagram) Build(w io.Writer) (err error) {
 
-	var minValue, maxValue float64
-	isFirst := true
-
-	for iCat := 0; iCat < len(d.categories); iCat++ {
-		if len(d.categories[iCat].Values) != len(d.labels) {
-			err = errors.New(fmt.Sprintf("Error: Count of values for category '%s' does not match labels count",
-				d.categories[iCat].Name))
-			return
-		}
-
-		for iVal := 0; iVal < len(d.categories[iCat].Values); iVal++ {
-			if maxValue < d.categories[iCat].Values[iVal] {
-				maxValue = d.categories[iCat].Values[iVal]
-			}
-			if isFirst {
-				minValue = d.categories[iCat].Values[iVal]
-				isFirst = false
-			} else if minValue > d.categories[iCat].Values[iVal] {
-				minValue = d.categories[iCat].Values[iVal]
-			}
-		}
-	}
-
-	s := svg.New(w)
-	s.Start(d.width, d.height)
-
-	// Title
-	s.Text(d.width/2, dsMarginTop/2, d.title,
-		fmt.Sprintf("text-anchor:middle;font-size:%d;fill:%s", dsTitleFontSize, dsTitleFontColor))
-
-	// Y axis
-	s.Line(dsMarginLeft, d.height-dsMarginBottom, dsMarginLeft, dsMarginTop,
-		fmt.Sprintf("stroke-width:%d;stroke:%s;", dsAxisLineWidth, dsAxisLineColor))
-	// X axis
-	s.Line(dsMarginLeft, d.height-dsMarginBottom, d.width-dsMarginRight, d.height-dsMarginBottom,
-		fmt.Sprintf("stroke-width:%d;stroke:%s;", dsAxisLineWidth, dsAxisLineColor))
-
-	// Write labels
-	length := len(d.labels)
-	xStep := (d.width - dsMarginLeft - dsMarginRight) / (length - 1)
-	left := dsMarginLeft
-
-	s.Group(fmt.Sprintf("text-anchor:middle;font-size:%d;fill:%s", dsLabelsFontSize, dsLabelsFontColor))
-	for i := 0; i < length; i++ {
-		s.Text(left, d.height-dsMarginBottom+dsLabelsMargin, d.labels[i])
-		left += xStep
-	}
-	s.Gend()
-
-	// Write Y values
-	// TODO round Y start for step nearest value
-	if d.vStep <= 0 {
-		err = errors.New(fmt.Sprintf("Error: Invalid VStep value '%d'", d.vStep))
+	if d.diagram == nil {
+		err = errors.New("Error: No diagram created")
 		return
 	}
-
-	yCount := float64(maxValue-minValue) * 1.1 / float64(d.vStep)
-	vCount := int(yCount + 0.5)
-	yStep := int(float64(d.height-dsMarginTop-dsMarginBottom) / yCount)
-	val := int(minValue)
-	top := d.height - dsMarginBottom
-
-	s.Group(fmt.Sprintf("text-anchor:end;font-size:%d;fill:%s", dsLabelsFontSize, dsLabelsFontColor))
-	for i := 0; i < vCount; i++ {
-		s.Text(dsMarginLeft-dsValuesMargin, top, fmt.Sprintf("%d", val))
-		val += d.vStep
-		top -= yStep
-	}
-	s.Gend()
-
-	// Drawing grid
-	if d.isGrid {
-
-		s.Group("stroke-width:1;stroke:lightgray")
-
-		// Vertical grid
-		left = dsMarginLeft + xStep
-		for i := 1; i < length; i++ {
-
-			s.Line(left, dsMarginTop, left, d.height-dsMarginBottom)
-			left += xStep
-		}
-
-		// Horizontal grid
-		top = d.height - dsMarginBottom - yStep
-		for i := 1; i < vCount; i++ {
-			s.Line(dsMarginLeft, top, d.width-dsMarginRight, top)
-			top -= yStep
-		}
-
-		s.Gend()
-	}
-
-	// Draw linear graphs
-
-	for iCat := 0; iCat < len(d.categories); iCat++ {
-
-		s.Group(fmt.Sprintf("stroke-width:%d;stroke:%s", dsLinearWidth, d.categories[iCat].Color))
-
-		valLength := maxValue - minValue
-		yLength := float64(yStep*(vCount-1))
-		x1 := dsMarginLeft
-		y1 := d.height - dsMarginBottom - int((d.categories[iCat].Values[0]-minValue)/valLength*yLength)
-
-		length = len(d.categories[iCat].Values)-1
-		for iVal := 0; iVal < length; iVal++ {
-
-			x2 := dsMarginLeft+(iVal+1)*xStep
-			y2 := d.height - dsMarginBottom - int((d.categories[iCat].Values[iVal+1]-d.categories[iCat].Values[0])/valLength*yLength)
-
-			s.Line(x1, y1, x2, y2)
-			fmt.Println(valLength, yLength, x1, y1, x2, y2)
-
-			y1 = y2
-			x1 = x2
-		}
-		s.Gend()
-	}
-
-	s.End()
+	err = d.diagram.build(w)
 
 	return
 }
